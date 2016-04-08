30e9658dd031ea5d257ca133a652ba5181769f29
Dmitriy Khodakov <dmitryhd@gmail.com>
2016-04-08 08:45:00 +0300

    Added angular js tests with jasmine

--- static/js/simple_app.js
+++ static/js/simple_app.js
@@ -1,4 +1,7 @@
 angular.module('simpleApp', [])
     .controller('SimpleCtrl', function($scope) {
         $scope.someVar = 100;
+        $scope.fn = function() {
+            return $scope.someVar;
+        }
     });
